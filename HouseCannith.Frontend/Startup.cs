835adb7a058bc5a98b8a52a274001871a4ae238a
Dan Bjorge <dan@dbjorge.net>
2017-07-18 20:04:27 -0700

    [doesn't build] migrate custom settings from .Broken

--- HouseCannith.Frontend/Startup.cs
+++ HouseCannith.Frontend/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HouseCannith.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -18,18 +21,38 @@ namespace HouseCannith_Frontend
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // How to connect to the comprendium database for local testing:
+                //  1) Add your dev box's IP to the firewall rules at portal.azure.com
+                //  2) Right click on the HouseCannith.WebApp project, Manage User Secrets, and add an entry as follows (filling in the password):
+                //       "COMPRENDIUM_DATABASE_CONNECTION_STRING": "Server=tcp:comprendium.database.windows.net,1433;Initial Catalog=comprendium;Persist Security Info=False;User ID=ComprendiumDev;Password=PASSWORD_FROM_KEYPASS;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+                //
+                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets<Startup>();
+            }
+
+            builder.AddEnvironmentVariables();
             Configuration = builder.Build();
         }
 
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // Stuff added here is generally consumed in Controllers via automatic constructor injection
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddAuthentication(
+                SharedOptions => SharedOptions.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var comprendiumDbConnectionString = Configuration["COMPRENDIUM_DATABASE_CONNECTION_STRING"];
+
+            services.AddDbContext<ComprendiumDbContext>(options => options.UseSqlServer(comprendiumDbConnectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +74,18 @@ namespace HouseCannith_Frontend
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseHttpToHttpsRedirect();
+
             app.UseStaticFiles();
+
+            app.UseCookieAuthentication();
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                ClientId = Configuration["Authentication:AzureAd:ClientId"],
+                Authority = Configuration["Authentication:AzureAd:AADInstance"] + Configuration["Authentication:AzureAd:TenantId"],
+                CallbackPath = Configuration["Authentication:AzureAd:CallbackPath"]
+            });
 
             app.UseMvc(routes =>
             {
@@ -63,6 +97,22 @@ namespace HouseCannith_Frontend
                     name: "spa-fallback",
                     defaults: new { controller = "Home", action = "Index" });
             });
+        }
+    }
+
+    public static class IApplicationBuilderHttpsExtensions
+    {
+        public static void UseHttpToHttpsRedirect(this IApplicationBuilder app)
+        {
+            app.UseWhen(
+                context => !context.Request.IsHttps,
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+                httpApp => httpApp.Use(async (context, next) =>
+                {
+                    var httpsUrl = "https://" + context.Request.Host + context.Request.Path;
+                    context.Response.Redirect(httpsUrl);
+                }));
+#pragma warning restore CS1998
         }
     }
 }
